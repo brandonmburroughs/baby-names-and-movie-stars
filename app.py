175dfc24481a3185e5d161e35d40641a83e537c4
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2016-02-20 20:39:54 -0500

    Adding new fields to segment data call

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
 from baby_names import baby_names_utilities
 from imdb_scraper import imdb_scraper
 
@@ -13,6 +13,16 @@ parser.add_argument('name', type=str, help="The 'name' parameter must be a strin
 parser.add_argument('gender', type=str, help="The 'gender' parameter must be a string!")
 parser.add_argument('year', type=int, help="The 'year' parameter must be a integer!")
 
+# Resource fields for output
+record_fields = { 
+    'year': fields.Float,
+    'count': fields.Float
+}
+
+record_list_fields = {
+    'records': fields.List(fields.Nested(record_fields))
+}
+
 # Load data
 baby_names_df = baby_names_utilities.load_baby_names_data()
 
@@ -22,11 +32,13 @@ class ActorPopularMovies(Resource):
         return imdb_scraper.get_actor_popular_movies(actor_name)
 
 class BabyNamesSegment(Resource):
-    def post(self):
+    #@marshal_with(record_list_fields)
+    def get(self):
         args = parser.parse_args()
         segment = baby_names_utilities.segment_data({'name':args['name'], 
-                                                     'gender':args['gender'], 
-                                                     'year':args['year']})
+                                                      'gender':args['gender'], 
+                                                     'year':args['year']},
+                                                     ['year','count'])
 
         segment_json = segment.to_json(orient='records')
         
@@ -35,11 +47,11 @@ class BabyNamesSegment(Resource):
 
 # Add APIs 
 api.add_resource(ActorPopularMovies, '/popular-movies/<string:actor_name>')
-api.add_resource(BabyNamesSegment, '/segment-baby-names/')
+api.add_resource(BabyNamesSegment, '/segment-baby-names')
 
 # App routing
 @app.route('/')
-def enter_name():
+def home():
     return render_template('index.html')
 
 
