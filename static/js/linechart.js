83eb00b360d5ec512da3587b2f18f0072d08bd41
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2016-02-21 22:22:36 -0500

    Added logic for multiple movies in one year

--- static/js/linechart.js
+++ static/js/linechart.js
@@ -19,6 +19,14 @@ function toProperCase(str) {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+// Sort JSON array
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
 // The function to run when the user is finished typing
 function generateChart () {
 
@@ -35,13 +43,28 @@ function generateChart () {
       // Parse baby name data
       $.getJSON('http://127.0.0.1:5000/segment-baby-names?name='.concat(actor_first_name), function (baby_data) {
 
+          // Sort movie data
+          movie_data = sortByKey(movie_data, 'x');
+
           // Populate series
           for (i = 0; i < baby_data.length; i++) {
             // Check for movies in each year
+            year = 0;
             for (j=0; j < movie_data.length; j++) {
-              // Check years
-              if (movie_data[j]['year'] == baby_data[i]['year']) {
-                movie_processed_json.push({'x': movie_data[j]['year'], 'y': baby_data[i]['count'], 'title': movie_data[j]['title'], 'link': movie_data[j]['link']})
+              // Find the years where movie data and baby data match.
+              // If they match, add the y value for the baby data.
+              // Also, look for cases when there were two movies for one year.
+              // If that's the case (else if), concatentate the two movies
+              // like Movie 1 & Move 2
+              if (movie_data[j]['year'] == baby_data[i]['year'] && movie_data[j]['year'] != year) {
+                // Add data
+                movie_processed_json.push({'x': movie_data[j]['year'], 'y': baby_data[i]['count'], 'title': '"' + movie_data[j]['title'] + '"', 'link': movie_data[j]['link']});
+                
+                // Set the year
+                year = movie_data[j]['year'];
+              } else if (movie_data[j]['year'] == baby_data[i]['year'] && movie_data[j]['year'] == year) {
+                // Concatenate titles
+                movie_processed_json[movie_processed_json.length-1]['title'] = movie_processed_json[movie_processed_json.length-1]['title'] + ' & "' + movie_data[j]['title'] + '"';
               }
             }
 
@@ -119,7 +142,7 @@ function generateChart () {
                   lineWidth: 0,
                   marker: {
                     enable: true,
-                    symbol: 'square',
+                    symbol: 'circle',
                     radius: 5
                   },
                   zIndex: 1
